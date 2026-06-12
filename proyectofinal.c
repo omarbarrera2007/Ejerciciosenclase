cdd95b01218f454625b490f2135d6c6c5e9a2e42
omarbarrera2007 <omarbarrera2007@gmail.com>
2026-06-12 01:50:48 +0000

    subir proyectofinal.c

--- proyectofinal.c
+++ proyectofinal.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #define FILAS 15
 #define COLUMNAS 21
@@ -14,11 +15,23 @@
 
 char mapa[FILAS][COLUMNAS + 1];
 int jugadorFila, jugadorCol, inicioJugadorFila, inicioJugadorCol;
+int velFila = 0;
+int velCol = 0;
 int fantasmaFila[MAX_FANTASMAS], fantasmaCol[MAX_FANTASMAS];
 int inicioFantasmaFila[MAX_FANTASMAS], inicioFantasmaCol[MAX_FANTASMAS];
 int fantasmaDir[MAX_FANTASMAS], totalFantasmas;
+int dirFantasma[MAX_FANTASMAS];
 int puntos = 0, vidas = 3, pellets = 0, nivel = 1;
+
+int dirJugador = 0;
+int framePikachu = 0;
+int frameGengar = 0;
+
+Uint32 ultimoFrame = 0;
+
 bool jugando = true, victoria = false;
+SDL_Texture *pikachuTexture = NULL;
+SDL_Texture *gengarTexture = NULL;
 
 char mapasDefault[TOTAL_NIVELES][FILAS][COLUMNAS + 1] = {
     {
@@ -131,6 +144,7 @@ void buscarElementos(void)
                 fantasmaFila[totalFantasmas] = inicioFantasmaFila[totalFantasmas] = f;
                 fantasmaCol[totalFantasmas] = inicioFantasmaCol[totalFantasmas] = c;
                 fantasmaDir[totalFantasmas] = rand() % 4;
+                dirFantasma[totalFantasmas] = 0;
                 totalFantasmas++;
                 mapa[f][c] = ' ';
             } else if (mapa[f][c] == '.') {
@@ -160,6 +174,11 @@ void moverJugador(int df, int dc)
 
     if (nf < 0 || nf >= FILAS || nc < 0 || nc >= COLUMNAS) return;
     if (mapa[nf][nc] == '#') return;
+
+    if(df == -1) dirJugador = 3;
+    if(df == 1)  dirJugador = 0;
+    if(dc == -1) dirJugador = 1;
+    if(dc == 1)  dirJugador = 2;
 
     jugadorFila = nf;
     jugadorCol = nc;
@@ -193,6 +212,11 @@ void moverFantasmas(void)
             if (fantasmaDir[i] == 1) nf++;
             if (fantasmaDir[i] == 2) nc--;
             if (fantasmaDir[i] == 3) nc++;
+
+            if (fantasmaDir[i] == 0) dirFantasma[i] = 3;
+            if (fantasmaDir[i] == 1) dirFantasma[i] = 0;
+            if (fantasmaDir[i] == 2) dirFantasma[i] = 1;
+            if (fantasmaDir[i] == 3) dirFantasma[i] = 2;
 
             if (nf >= 0 && nf < FILAS && nc >= 0 && nc < COLUMNAS && mapa[nf][nc] != '#') {
                 fantasmaFila[i] = nf;
@@ -264,14 +288,42 @@ void renderizar(SDL_Renderer *renderer)
         }
     }
 
-    SDL_SetRenderDrawColor(renderer, 255, 230, 0, 255);
-    dibujarCirculo(renderer, jugadorCol * TAM + TAM / 2, jugadorFila * TAM + TAM / 2, 12);
+   SDL_Rect pikachuDestino = {
+    jugadorCol * TAM-8,
+    jugadorFila * TAM-8,
+    48,
+    48
+};
 
-    SDL_SetRenderDrawColor(renderer, 255, 60, 90, 255);
+SDL_Rect pikachuFrame = {
+    framePikachu * 64, dirJugador * 64, 64, 64
+};
+
+
+SDL_RenderCopy(renderer,
+               pikachuTexture,
+               &pikachuFrame,
+               &pikachuDestino);
+
     for (i = 0; i < totalFantasmas; i++) {
-        SDL_Rect fantasma = {fantasmaCol[i] * TAM + 5, fantasmaFila[i] * TAM + 5, TAM - 10, TAM - 10};
-        SDL_RenderFillRect(renderer, &fantasma);
-    }
+
+    SDL_Rect gengarDestino = {
+        fantasmaCol[i] * TAM-8,
+        fantasmaFila[i] * TAM-8,
+        48,
+        48
+    };
+
+    SDL_Rect gengarFrame = {
+    frameGengar * 48, dirFantasma[i] * 48, 48, 48
+};
+
+
+    SDL_RenderCopy(renderer,
+                   gengarTexture,
+                   &gengarFrame,
+                   &gengarDestino);
+}
 
     SDL_RenderPresent(renderer);
 }
@@ -282,7 +334,7 @@ void actualizarTitulo(SDL_Window *window)
     sprintf(titulo, "Pac-Man | Nivel: %d/3 | Vidas: %d | Puntos: %d", nivel, vidas, puntos);
     SDL_SetWindowTitle(window, titulo);
 }
-
+Uint32 ultimoMovimientoJugador = 0;
 int main(int argc, char **argv)
 {
     SDL_Window *window;
@@ -297,8 +349,33 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+    printf("Error SDL_image: %s\n", IMG_GetError());
+    return 1;
+}
+
     window = SDL_CreateWindow("Pac-Man", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface *temp;
+
+temp = IMG_Load("pikachu.png");
+
+if (temp == NULL) {
+    printf("Error cargando pikachu.png: %s\n", IMG_GetError());
+} else {
+    pikachuTexture = SDL_CreateTextureFromSurface(renderer, temp);
+    SDL_FreeSurface(temp);
+}
+
+temp = IMG_Load("gengar.png");
+
+if (temp == NULL) {
+    printf("Error cargando gengar.png: %s\n", IMG_GetError());
+} else {
+    gengarTexture = SDL_CreateTextureFromSurface(renderer, temp);
+    SDL_FreeSurface(temp);
+}
 
     if (window == NULL || renderer == NULL) {
         printf("Error al crear ventana o renderer: %s\n", SDL_GetError());
@@ -315,22 +392,58 @@ int main(int argc, char **argv)
 
             if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_ESCAPE || event.key.keysym.sym == SDLK_x) jugando = false;
-                if (event.key.keysym.sym == SDLK_UP) moverJugador(-1, 0);
-                if (event.key.keysym.sym == SDLK_DOWN) moverJugador(1, 0);
-                if (event.key.keysym.sym == SDLK_LEFT) moverJugador(0, -1);
-                if (event.key.keysym.sym == SDLK_RIGHT) moverJugador(0, 1);
+                if (event.key.keysym.sym == SDLK_UP) {
+                        velFila = -1;
+                        velCol = 0;
+                }
+
+                if (event.key.keysym.sym == SDLK_DOWN) {
+                        velFila = 1;
+                        velCol = 0;
+                }
+
+                if (event.key.keysym.sym == SDLK_LEFT) {
+                            velFila = 0;
+                        velCol = -1;
+                }
+
+            if (event.key.keysym.sym == SDLK_RIGHT) {
+                        velFila = 0;
+                        velCol = 1;
+            }   
             }
         }
+if(SDL_GetTicks() - ultimoFrame > 120)
+{
+    framePikachu = (framePikachu + 1) % 4;
+    frameGengar = (frameGengar + 1) % 3;
 
+    ultimoFrame = SDL_GetTicks();
+}
         if (SDL_GetTicks() > ultimoFantasma + 300) {
             moverFantasmas();
             ultimoFantasma = SDL_GetTicks();
         }
 
+        if(SDL_GetTicks() - ultimoMovimientoJugador > 180)
+{
+    moverJugador(velFila, velCol);
+    ultimoMovimientoJugador = SDL_GetTicks();
+}
+
         revisarColisiones();
 
-        if (vidas <= 0) {
-            jugando = false;
+        if (vidas <= 0)
+{
+    SDL_ShowSimpleMessageBox(
+        SDL_MESSAGEBOX_ERROR,
+        "CHEPAMON",
+        "Has perdido todas tus vidas.",
+        window
+    );
+
+    jugando = false;
+
         } else if (pellets <= 0) {
             if (nivel < TOTAL_NIVELES) {
                 nivel++;
@@ -339,7 +452,15 @@ int main(int argc, char **argv)
                 SDL_Delay(500);
             } else {
                 victoria = true;
-                jugando = false;
+
+SDL_ShowSimpleMessageBox(
+    SDL_MESSAGEBOX_INFORMATION,
+    "CHEPAMON",
+    "Felicidades, completaste todos los niveles.",
+    window
+);
+
+jugando = false;
             }
         }
 
@@ -357,6 +478,11 @@ int main(int argc, char **argv)
     }
 
     printf("Puntaje final: %d\n", puntos);
+
+    SDL_DestroyTexture(pikachuTexture);
+    SDL_DestroyTexture(gengarTexture);
+
+    IMG_Quit();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
